ec6b51808528978e5e2544e73a11d2dd00e59747
rajneeshkumar1507 <84806890+rajneeshkumar1507@users.noreply.github.com>
2022-09-10 14:37:23 +0530

    Initial Commit

---
+++ reverseInteger.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+using namespace std;
+int reverseInteger(int n)
+{
+    int ans=0;
+    while(n!=0)
+    {
+        int digit=n%10;
+        if((ans>INT_MAX/10) || (ans<INT_MIN/10))
+        {
+            return 0;
+        }
+        ans = (ans * 10) + digit;
+        n = n /10;
+    }
+    return ans;
+}
+int main()
+{
+    int n;
+    cout<<"Enter the Number"<<endl;
+    cin>>n;
+    cout<<reverseInteger(n);
+    return 0;
+}
